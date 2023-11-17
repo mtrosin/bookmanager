3578eb11d8641cd10ac40f526119c62e6ad06c2f
Matheus Rosin <mtrosin@gmail.com>
2023-11-17 19:13:58 -0300

    added the connection string

---
+++ BookManager/BookManager.API/Data/BookManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookManager.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManager.API.Data
+{
+    public class BookManagerDbContext: DbContext
+    {
+        public BookManagerDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
--- BookManager/BookManager.API/Program.cs
+++ BookManager/BookManager.API/Program.cs
@@ -1,3 +1,4 @@
+using BookManager.API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookManagerDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookManagerConnectionString"))
+);
 
 var app = builder.Build();
 
