f96342e63abbbda7c3a56da1651fc220c036622b
Matheus Rosin <mtrosin@gmail.com>
2023-11-17 19:13:03 -0300

    added Book and Author models

---
+++ BookManager/BookManager.API/Models/Domain/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace BookManager.API.Models.Domain
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ BookManager/BookManager.API/Models/Domain/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookManager.API.Models.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int Year {  get; set; }
+        public string? CoverImage { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
