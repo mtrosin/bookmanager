88ee49f52baaa348b8ac695e4610a11fffc4cd67
Matheus Rosin <mtrosin@gmail.com>
2023-11-18 18:35:45 -0300

    added image table migration

--- BookManager/BookManager.API/Migrations/20231118192030_adding images table.Designer.cs
+++ BookManager/BookManager.API/Migrations/20231118192030_adding images table.Designer.cs
@@ -3,6 +3,7 @@ using BookManager.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookManager.API.Migrations
 {
     [DbContext(typeof(BookManagerDbContext))]
-    partial class BookManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231118192030_adding images table")]
+    partial class addingimagestable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
